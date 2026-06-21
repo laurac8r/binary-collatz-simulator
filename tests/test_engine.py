af188a74c8b7acfea8f9bfb4a72519756ec2224e
laura <laurakaminskiy@webcatgirl.com>
2026-06-21 15:03:29 -0400

    feat: enhance `TestIterate` with parameterized tests for accuracy

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,14 +1,18 @@
+import pytest
+
 from src.binary_collatz.engine import iterate
 
 
 class TestIterate:
-    def test_27_returns_111_steps_9232_peak(self):
+    @pytest.mark.parametrize("n, lgt, peak", [(27, 111, 9232), (6, 8, 16), (1, 0, 0)])
+    def test_accurate_num_and_peak(self, n, lgt, peak):
         steps = []
 
-        step = 27
+        step = n
 
         for step in iterate(step):
             steps.append(step)
 
-        assert len(steps) == 111
-        assert max([x.value for x in steps]) == 9232
+        assert len(steps) == lgt
+        if lgt > 0:
+            assert max([x.value for x in steps]) == peak
